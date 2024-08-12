77538fcfc0544104dc8eb2455b6f1c3781cd7711
nekowawolf <irukainu20@gmail.com>
2024-08-12 20:43:01 +0700

    update: dropdown add dropdownItems

--- public/js/airdrop-dropdown.js
+++ public/js/airdrop-dropdown.js
@@ -1,25 +1,34 @@
 const dropdownButton = document.getElementById('hs-dropdown-default');
 const dropdownMenu = document.querySelector('.hs-dropdown-menu');
 const arrow = document.getElementById('dropdown-arrow');
+const dropdownItems = document.querySelectorAll('.dropdown-item');
 
 dropdownButton.addEventListener('click', function() {
-    // Toggle visibility of dropdown
     dropdownMenu.classList.toggle('hidden');
     dropdownMenu.classList.toggle('opacity-0');
     dropdownMenu.classList.toggle('hs-dropdown-open:opacity-100');
     
-    // Toggle rotation of arrow
     arrow.classList.toggle('rotate-180');
 });
 
-// Optional: Close the dropdown when clicking outside of it
+
+dropdownItems.forEach(item => {
+    item.addEventListener('click', function() {
+        dropdownMenu.classList.add('hidden');
+        dropdownMenu.classList.add('opacity-0');
+        dropdownMenu.classList.remove('hs-dropdown-open:opacity-100');
+        
+        arrow.classList.remove('rotate-180');
+    });
+});
+
+
 window.addEventListener('click', function(event) {
     if (!dropdownButton.contains(event.target) && !dropdownMenu.contains(event.target)) {
         dropdownMenu.classList.add('hidden');
         dropdownMenu.classList.add('opacity-0');
         dropdownMenu.classList.remove('hs-dropdown-open:opacity-100');
         
-        // Reset arrow rotation
         arrow.classList.remove('rotate-180');
     }
 });
