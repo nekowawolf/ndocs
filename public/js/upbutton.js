f207673985a5fea15eec354b308e3a3ea5329038
nekowawolf <irukainu20@gmail.com>
2024-08-15 06:58:17 +0700

    update: airdroplist add upbutton

--- public/js/upbutton.js
+++ public/js/upbutton.js
@@ -1,20 +1,13 @@
-// Ambil tombol
-const mybutton = document.getElementById("btn-back-to-top");
+const backToTopButton = document.getElementById('back-to-top');
 
-// Fungsi untuk menampilkan atau menyembunyikan tombol
-const scrollFunction = () => {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.classList.remove("hidden");
-  } else {
-    mybutton.classList.add("hidden");
-  }
-};
+        window.onscroll = function() {
+            if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+                backToTopButton.style.display = "block";
+            } else {
+                backToTopButton.style.display = "none";
+            }
+        };
 
-// Fungsi untuk kembali ke atas
-const backToTop = () => {
-  window.scrollTo({ top: 0, behavior: "smooth" });
-};
-
-// Tambahkan event listener
-mybutton.addEventListener("click", backToTop);
-window.addEventListener("scroll", scrollFunction);
+        backToTopButton.addEventListener('click', function() {
+            window.scrollTo({ top: 0, behavior: 'smooth' });
+        });
